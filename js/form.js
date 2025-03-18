b49d8a54375016cbdd6fa4bfb8390afbbb023b96
Pytwer.Dev <122356458+Pytwer@users.noreply.github.com>
2025-03-18 14:19:19 -0300

    teste

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+//botões do trilha
 document.querySelectorAll('.trilha-option input[type="radio"]').forEach((radio) => {
     radio.addEventListener('change', function () {
         // Remove a classe de seleção de todas as opções
@@ -12,29 +13,30 @@ document.querySelectorAll('.trilha-option input[type="radio"]').forEach((radio) 
         
     });
 });
+// validação de cpf 
+let cpf = document.querySelector("#cpf");
 
+cpf.addEventListener("blur", function(){
+   if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
+});
 
-
+//botão de enviar.
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('inscricaoForm');
     const modal = document.getElementById('confirmationModal');
     const closeModalButton = document.getElementById('closeModal');
-
     // Função para exibir o modal
     function showModal() {
         modal.style.display = 'flex';
     }
-
     // Função para fechar o modal e redirecionar
     function closeModal() {
         modal.style.display = 'none';
         window.location.href = '../index.html'; // Redireciona para a home
     }
-
     // Evento de envio do formulário
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // Impede o envio padrão do formulário
-
         // Validação dos campos do formulário
         const allFieldsValid = Array.from(form.elements).every(element => {
             if (element.required && !element.value.trim()) {
@@ -42,14 +44,12 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             return true;
         });
-
         if (allFieldsValid) {
             showModal(); // Exibe o modal se todos os campos estiverem preenchidos
         } else {
             alert('Por favor, preencha todos os campos obrigatórios.');
         }
     });
-
     // Evento de clique no botão "Fechar" do modal
     closeModalButton.addEventListener('click', closeModal);
 });
