f71dbc63fefd4f13ee09eb2aa6b8eb74a98c76cd
Pytwer.Dev <122356458+Pytwer@users.noreply.github.com>
2025-03-19 07:58:27 -0300

    teste

--- js/form.js
+++ js/form.js
@@ -53,25 +53,86 @@ document.addEventListener('DOMContentLoaded', function () {
     closeModalButton.addEventListener('click', closeModal);
 });
 
-var dateControl = document.querySelector('input[type="date"]');
-dateControl.value = "2025-03-18";
+// validação de numero 
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+  }
+  
+  const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.substring(0,11)
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+  }
 
-//validação telefone.
-function inputHandler(masks, max, event) {
-	var c = event.target;
-	var v = c.value.replace(/\D/g, '');
-	var m = c.value.length > max ? 1 : 0;
-	VMasker(c).unMask();
-	VMasker(c).maskPattern(masks[m]);
-	c.value = VMasker.toPattern(v, masks[m]);
+// validação de email 
+function validacaoEmail(field) {
+    const email = field.value.trim();  // Remove espaços no início e no fim
+    const usuario = email.substring(0, email.indexOf("@"));
+    const dominio = email.substring(email.indexOf("@") + 1);
+
+    // Validações:
+    if (
+        usuario.length >= 1 &&  // usuário não pode ser vazio
+        dominio.length >= 3 &&  // domínio deve ter ao menos 3 caracteres
+        usuario.search(" ") === -1 &&  // usuário não pode ter espaços
+        dominio.search(" ") === -1 &&  // domínio não pode ter espaços
+        dominio.search(".") !== -1 &&  // domínio deve ter pelo menos um ponto
+        dominio.indexOf(".") >= 1 &&  // o ponto não pode estar no início
+        dominio.lastIndexOf(".") < dominio.length - 1 // o ponto não pode estar no final
+    ) {
+        // Se for válido:
+        document.getElementById("msgemail").innerHTML = "E-mail válido";
+        document.getElementById("msgemail").style.color = "green";  // Mensagem verde
+    } else {
+        // Se for inválido:
+        document.getElementById("msgemail").innerHTML = "<font color='red'>E-mail inválido </font>";
+        document.getElementById("msgemail").style.color = "red";  // Mensagem vermelha
+    }
 }
 
-var telMask = ['(99) 9999-99999', '(99) 99999-9999'];
-var tel = document.querySelector('input[attrname=telephone1]');
-VMasker(tel).maskPattern(telMask[0]);
-tel.addEventListener('input', inputHandler.bind(undefined, telMask, 14), false);
+// validação de cep 
 
-var docMask = ['999.999.999-999', '99.999.999/9999-99'];
-var doc = document.querySelector('#doc');
-VMasker(doc).maskPattern(docMask[0]);
-doc.addEventListener('input', inputHandler.bind(undefined, docMask, 14), false);
+const handleZipCode = (event) => {
+    let input = event.target
+    input.value = zipCodeMask(input.value)
+  }
+  
+  const zipCodeMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.substring(0,8)
+    value = value.replace(/(\d{5})(\d)/,'$1-$2')
+    return value
+  }
+
+//Seleção de checkbox
+function selecionarCheckbox(checkbox) {
+    // Seleciona todos os checkboxes dentro da div container
+    var checkboxes = document.querySelectorAll('.container input[type="checkbox"]');
+
+    // Se o checkbox foi marcado, desmarca todos os outros
+    checkboxes.forEach(function(item) {
+        if (item !== checkbox) {
+            item.checked = false; // Desmarca os outros checkboxes
+        }
+    });
+}
+
+// inscrição 
+
+function mensagem()
+{
+    alert("Sua inscrição foi feita como sucesso");
+}
+
+window.onload=function(){
+    let botaoMensagem = document.getElementById("mensagem");
+
+    botaoMensagem.onclick = function () {
+        mensagem() ;
+    };
+}
